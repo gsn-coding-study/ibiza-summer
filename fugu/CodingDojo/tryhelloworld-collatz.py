5e3167d7225984a0dadfcf38c975b7eb8e408acd
june26 <june26@gscdn.com>
2017-07-05 13:53:57 +0900

    add coding exam

---
+++ fugu/CodingDojo/tryhelloworld-collatz.py
@@ -0,0 +1,9 @@
+def collatz(num):    
+    for i in range(500):    
+        num = num/2 if 0==num%2 else num*3+1
+        if 1==num:
+            return i+1
+    return -1
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(collatz(6))
