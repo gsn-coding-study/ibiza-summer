abf9869bbdab9e8fb810b3fa91e6cdff81173ec1
Coach <mikionpub@gmail.com>
2017-10-11 18:06:43 +0900

    log

--- coach/Doit_nodejs/2.modules/2-4.modules.js
+++ coach/Doit_nodejs/2.modules/2-4.modules.js
@@ -1,3 +1,5 @@
 var calc = require('./2-4.calc');
 
-console.log('모듈로 분리하기 전 - calc.add 함수 호출 결과 : %d', calc.add(10,10));
+//console.log('모듈로 분리하기 전 - calc.add 함수 호출 결과 : %d', calc.add(10,10));
+
+console.log('모듈로 분리하고 - calc.add 함수 호출 결과 : %d', calc.add(10,10));
